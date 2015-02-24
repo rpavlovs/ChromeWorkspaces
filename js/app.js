d8b26497eb4abeb1c8b45e3a1d488b44eb028ba2
Roman Pavlovskyi <wilohvost@gmail.com>
2015-02-23 19:18:39 -0500

    restructure file-tree, add basic calls to Chrome tabs API

--- js/app.js
+++ js/app.js
@@ -1,7 +1,41 @@
-var app = angular.module('ChromeWorkspaces', []);
+angular.module('ChromeWorkspaces', [])
 
-app.controller('WorkspaceCtrl', ['$scope', function($scope) {
-	$scope.workspaces = [];
-	$scope.workspaces.push({name: "first WS"});
+.factory('wsFactory', function() {
+
+	var workspaces = [];
+
+	return {
+		get: function get () {
+			return workspaces;
+		},
+		add: function add (name) {
+			var ws = {};
+
+			ws.name = name;
+			chrome.tabs.query({currentWindow: true}, function(tabs) {
+				ws.tabs = tabs;
+				workspaces.push(ws);
+			});
+
+			console.log(ws);
+			console.log(workspaces);
+		}
+	};
+
+})
+
+.controller('WorkspaceCtrl', ['$scope', 'wsFactory', function($scope, wsFactory) {
+	$scope.isSaved = false;
+	$scope.workspaces = wsFactory.get();
+
+	$scope.save = function() {
+		// $scope.isSaved = true;
+		wsFactory.add('a workspace');
+	};
 
 }]);
+
+
+// chrome.storage.sync.get( 'contexts', function(items) {
+
+// chrome.storage.sync.set( {'contexts': items.contexts} );
